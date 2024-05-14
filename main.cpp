53d587f765542edfb648879d8e5d0063b82c7158
Ani <ansahakyan1500@gmail.com>
2024-05-14 22:22:35 +0400

    Task2: Дана последовательность целых чисел. Создать новую последовательность, заменив все числа в диапазоне от первого четного до последнего нечетного минимальным числом. Создать новую последовательность, применив циклический сдвиг так, чтобы на первом месте стоял максимальный элемент. Отсортировать последовательность, удалив повторяющиеся элеметны.

--- main.cpp
+++ main.cpp
@@ -1,39 +1,70 @@
 #include <iostream>
-#include <vector>
+#include <deque>
 #include <algorithm>
 #include <numeric>
-#include <locale> // ƒобавлен заголовок дл€ setlocale
 using namespace std;
-
-
 
 int main() {
     setlocale(LC_ALL, "russian");
-    int n;//кол-во чисел в строке
+    int n; // кол-во чисел в последовательности
     cout << "n = ";
     cin >> n;
-    vector<int> vec;//вектор дл€ хранени€ целочисленных значений 
+    deque<int> sequence; // дек дл€ хранени€ целочисленных значений
 
     int x;
-    for (int i = 0; i < n; i++) {//заполнение вектора 
+    for (int i = 0; i < n; i++) {
         cout << "a[" << i << "] = ";
         cin >> x;
-        vec.push_back(x);//добавл€ет элемент в конец вектора 
+        sequence.push_back(x); // добавл€ет элемент в конец 
     }
 
-    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++) {//ќбращение к элементу, на который указывает итератор : *iter.
-        cout << *iter << " ";//вывод вектора
+    for (deque<int>::iterator iter = sequence.begin(); iter != sequence.end(); iter++) {
+        cout << *iter << " "; // вывод последовательности
     }
     cout << endl;
 
 
 
-    cout << "«амена минимальных чисел нулем:\n";
-    int Min = *min_element(vec.begin(), vec.end());//находим минимальный эл
-    replace(vec.begin(), vec.end(), Min, 0);//замен€ем нулем 
+    deque<int> replacedSequence(sequence); // —оздание копии исходной последовательности (полна€ независима€)
 
-    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++) {//выводим 
-        cout << *iter << " ";
+    // Ќахождение первого четного и последнего нечетного чисел
+    auto firstEven = find_if(replacedSequence.begin(), replacedSequence.end(), [](int x) { return x % 2 == 0; });
+    auto lastOdd = find_if(replacedSequence.rbegin(), replacedSequence.rend(), [](int x) { return x % 2 != 0; }).base();
+
+    // «амена чисел в диапазоне от первого четного до последнего нечетного минимальным числом
+    auto minNumber = *min_element(firstEven, lastOdd);
+    for_each(firstEven, lastOdd, [minNumber](int& x) { x = minNumber; });
+
+    deque<int> rotatedSequence(sequence); // —оздание копии исходной последовательности
+
+    // Ќахождение максимального элемента
+    auto maxElement = max_element(rotatedSequence.begin(), rotatedSequence.end());
+
+    // ÷иклический сдвиг так, чтобы на первом месте сто€л максимальный элемент
+    rotate(rotatedSequence.begin(), maxElement, rotatedSequence.end());
+
+    deque<int> sortedUniqueSequence(sequence); // —оздание копии исходной последовательности
+
+    // ”даление повтор€ющихс€ элементов и сортировка
+    sort(sortedUniqueSequence.begin(), sortedUniqueSequence.end());
+    sortedUniqueSequence.erase(unique(sortedUniqueSequence.begin(), sortedUniqueSequence.end()), sortedUniqueSequence.end());
+
+    // ¬ывод результатов
+    cout << "ѕоследовательность, где заменены все числа в диапазоне от первого четного до последнего нечетного минимальным числом: ";
+    for (int x : replacedSequence) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    cout << "ѕоследовательность, где будет применен циклический сдвиг так, чтобы на первом месте сто€л максимальный элемент: ";
+    for (int x : rotatedSequence) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    cout << "ќтсортированна€ последовательность, где удалены  повтор€ющиес€ элементы: ";
+    for (int x : sortedUniqueSequence) {
+        cout << x << " ";
     }
     cout << endl;
 
