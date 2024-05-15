fba4ce3eff06ae5798e4b25f7dcacfe6879046b9
Ani <ansahakyan1500@gmail.com>
2024-05-15 23:08:29 +0400

    Дан текст, состоящий из предложений, разделенных знаками препинания из набора «.?!». Предложения в свою очередь состоят из слов, отделенных друг от друга пробелами. Найти слова (без учета регистра), которые встречаются только в повествовательных предложениях.

--- main.cpp
+++ main.cpp
@@ -1,58 +1,59 @@
 //#include <iostream>
-//#include <list>
-//#include <algorithm>
-//#include <functional>
-//#include <iterator>
 //#include <set>
-//#include <fstream>
-//#include <numeric>
-//#include <vector>
-//
+//#include <map>
+//#include <algorithm>
+//#include <iterator>
+//#include <string>
 //using namespace std;
-////bool even(int x) {
-//// return x % 2 == 0;
-////}
+//
+//string str_tolower(string s)
+////Функция преобразует строку s в нижний регистр
+//{
+//    for (int i = 0; i < s.length(); i++)
+//        s[i] = tolower(s[i]);
+//    return s;
+//}
+//
+//set <string> set_word(string s)
+////Функция извлекает множество из предложения s
+//{
+//    set <string> set_s;
+//    if (s.empty()) {
+//        return set_s;
+//    }
+//    if (s[s.length() - 1] != ' ')
+//        s += " ";
+//    while (s != "") {
+//        set_s.insert(str_tolower(s.substr(0, s.find(' '))));//добавл значения
+//        s.erase(0, s.find(' ') + 1);
+//    }
+//    return set_s;
+//}
 //
 //int main() {
-//	setlocale(LC_ALL, "Ru");
-//	set<int> two, rest; // для хранения только двухзнач / остальных цифр
-//	int n = 3, x;
-//	// считывание 10 чисел
-//	for (int i = 0; i < n; ++i) {
-//		cin >> x;
-//
-//		if (x > 9 && x < 100) { // если двухзнач
-//			while (x > 0) {
-//				two.insert(x % 10); // запоминаем каждую цифру
-//				x /= 10;
-//			}
-//		}
-//		else {
-//			while (x > 0) {
-//				rest.insert(x % 10);
-//				x /= 10;
-//			}
-//		}
-//	}
-//	// вывод цифр только в двузнач
-//	cout << "двузначные: ";
-//	for (auto it = two.begin(); it != two.end(); ++it)
-//		cout << *it << " ";
-//	cout << endl;
-//	// вывод остальных цифр
-//	cout << "Остальные : ";
-//	for (auto it = rest.begin(); it != rest.end(); ++it)
-//		cout << *it << " ";
-//	cout << endl;
-//	// поиск и вывод цифр только в трехзнач
-//	vector<int> v(two.size() + rest.size(), -1);
-//	set_difference(two.begin(), two.end(), rest.begin(), rest.end(), v.begin());
-//	v.erase(find(v.begin(), v.end(), -1), v.end());
-//
-//	cout << "Толко в двузначных: ";
-//	for (auto it = v.begin(); it != v.end(); ++it)
-//		cout << *it << " ";
-//	cout << endl;
-//	system("pause");
-//	return 0;
+//    setlocale(LC_ALL, "RU");
+//    string s, pr;
+//    cout << "Введите текст\n";
+//    getline(cin, s);
+//    set <string> res, pov, voskl, vopr, s0;
+//    //Будем составлять множеста pov, voskl, vopr из извлеченных
+//    //повествовательных, восклицательных и вопросительных предложений соответственно
+//    while (s != "") {
+//        int pos = s.find_first_of(".!?");
+//        pr = s.substr(0, pos);
+//        s0 = set_word(pr);
+//        if (s[pos] == '.') pov.insert(s0.begin(), s0.end());
+//        if (s[pos] == '!') voskl.insert(s0.begin(), s0.end());
+//        if (s[pos] == '?') vopr.insert(s0.begin(), s0.end());
+//        s.erase(0, pos + 1);
+//    }
+//    // Находим объединение voskl и vopr
+//    set <string> set_vopr_voskl;
+//    set_vopr_voskl.insert(voskl.begin(), voskl.end());
+//    set_vopr_voskl.insert(vopr.begin(), vopr.end());
+//    // Находим разность pov и объединенного множеста
+//    set_difference(pov.begin(), pov.end(), set_vopr_voskl.begin(), set_vopr_voskl.end(), inserter(res, res.begin()));
+//    for (set <string> ::iterator i = res.begin(); i != res.end(); i++)
+//        cout << *i << " ";
+//    return 0;
 //}
