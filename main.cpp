b2598382e25c4dbf102ddf5dcc3be7eeb2e906fd
Ani <ansahakyan1500@gmail.com>
2024-05-15 23:07:31 +0400

    Задачи на set и map. Дана последовательность целых чисел. Найти все такие цифры, которые встречаются только в двузначных числах. Работать только с цифрами, которые встречаются в данной последовательности, а не со всеми цифрами от 0 до 9.

--- main.cpp
+++ main.cpp
@@ -1,74 +1,58 @@
-#include <iostream>
-#include <deque>
-#include <algorithm>
-#include <numeric>
-using namespace std;
-
-int main() {
-    setlocale(LC_ALL, "russian");
-    int n; // кол-во чисел в последовательности
-    cout << "n = ";
-    cin >> n;
-    deque<int> sequence; // дек для хранения целочисленных значений
-
-    int x;
-    for (int i = 0; i < n; i++) {
-        cout << "a[" << i << "] = ";
-        cin >> x;
-        sequence.push_back(x); // добавляет элемент в конец 
-    }
-
-    for (deque<int>::iterator iter = sequence.begin(); iter != sequence.end(); iter++) {
-        cout << *iter << " "; // вывод последовательности
-    }
-    cout << endl;
-
-
-
-    deque<int> replacedSequence(sequence); // Создание копии исходной последовательности (полная независимая)
-
-    // Нахождение первого четного и последнего нечетного чисел
-    auto firstEven = find_if(replacedSequence.begin(), replacedSequence.end(), [](int x) { return x % 2 == 0; });
-    auto lastOdd = find_if(replacedSequence.rbegin(), replacedSequence.rend(), [](int x) { return x % 2 != 0; }).base();
-
-    // Замена чисел в диапазоне от первого четного до последнего нечетного минимальным числом
-    auto minNumber = *min_element(firstEven, lastOdd);
-    for_each(firstEven, lastOdd, [minNumber](int& x) { x = minNumber; });
-
-    deque<int> rotatedSequence(sequence); // Создание копии исходной последовательности
-
-    // Нахождение максимального элемента
-    auto maxElement = max_element(rotatedSequence.begin(), rotatedSequence.end());
-
-    // Циклический сдвиг так, чтобы на первом месте стоял максимальный элемент
-    rotate(rotatedSequence.begin(), maxElement, rotatedSequence.end());
-
-    deque<int> sortedUniqueSequence(sequence); // Создание копии исходной последовательности
-
-    // Удаление повторяющихся элементов и сортировка
-    sort(sortedUniqueSequence.begin(), sortedUniqueSequence.end());
-    sortedUniqueSequence.erase(unique(sortedUniqueSequence.begin(), sortedUniqueSequence.end()), sortedUniqueSequence.end());
-
-    // Вывод результатов
-    cout << "Последовательность, где заменены все числа в диапазоне от первого четного до последнего нечетного минимальным числом: ";
-    for (int x : replacedSequence) {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    cout << "Последовательность, где будет применен циклический сдвиг так, чтобы на первом месте стоял максимальный элемент: ";
-    for (int x : rotatedSequence) {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    cout << "Отсортированная последовательность, где удалены  повторяющиеся элементы: ";
-    for (int x : sortedUniqueSequence) {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
-
-
+//#include <iostream>
+//#include <list>
+//#include <algorithm>
+//#include <functional>
+//#include <iterator>
+//#include <set>
+//#include <fstream>
+//#include <numeric>
+//#include <vector>
+//
+//using namespace std;
+////bool even(int x) {
+//// return x % 2 == 0;
+////}
+//
+//int main() {
+//	setlocale(LC_ALL, "Ru");
+//	set<int> two, rest; // для хранения только двухзнач / остальных цифр
+//	int n = 3, x;
+//	// считывание 10 чисел
+//	for (int i = 0; i < n; ++i) {
+//		cin >> x;
+//
+//		if (x > 9 && x < 100) { // если двухзнач
+//			while (x > 0) {
+//				two.insert(x % 10); // запоминаем каждую цифру
+//				x /= 10;
+//			}
+//		}
+//		else {
+//			while (x > 0) {
+//				rest.insert(x % 10);
+//				x /= 10;
+//			}
+//		}
+//	}
+//	// вывод цифр только в двузнач
+//	cout << "двузначные: ";
+//	for (auto it = two.begin(); it != two.end(); ++it)
+//		cout << *it << " ";
+//	cout << endl;
+//	// вывод остальных цифр
+//	cout << "Остальные : ";
+//	for (auto it = rest.begin(); it != rest.end(); ++it)
+//		cout << *it << " ";
+//	cout << endl;
+//	// поиск и вывод цифр только в трехзнач
+//	vector<int> v(two.size() + rest.size(), -1);
+//	set_difference(two.begin(), two.end(), rest.begin(), rest.end(), v.begin());
+//	v.erase(find(v.begin(), v.end(), -1), v.end());
+//
+//	cout << "Толко в двузначных: ";
+//	for (auto it = v.begin(); it != v.end(); ++it)
+//		cout << *it << " ";
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//}
