e9c94422102db88626aab7ce8e7b63d5ce30c6c6
Ani <ansahakyan1500@gmail.com>
2024-05-16 22:20:27 +0400

    Сортировки данных: быстрая сортировка

--- main.cpp
+++ main.cpp
@@ -1,59 +1,72 @@
-//#include <iostream>
-//#include <set>
-//#include <map>
-//#include <algorithm>
-//#include <iterator>
-//#include <string>
-//using namespace std;
-//
-//string str_tolower(string s)
-////Функция преобразует строку s в нижний регистр
-//{
-//    for (int i = 0; i < s.length(); i++)
-//        s[i] = tolower(s[i]);
-//    return s;
-//}
-//
-//set <string> set_word(string s)
-////Функция извлекает множество из предложения s
-//{
-//    set <string> set_s;
-//    if (s.empty()) {
-//        return set_s;
-//    }
-//    if (s[s.length() - 1] != ' ')
-//        s += " ";
-//    while (s != "") {
-//        set_s.insert(str_tolower(s.substr(0, s.find(' '))));//добавл значения
-//        s.erase(0, s.find(' ') + 1);
-//    }
-//    return set_s;
-//}
-//
-//int main() {
-//    setlocale(LC_ALL, "RU");
-//    string s, pr;
-//    cout << "Введите текст\n";
-//    getline(cin, s);
-//    set <string> res, pov, voskl, vopr, s0;
-//    //Будем составлять множеста pov, voskl, vopr из извлеченных
-//    //повествовательных, восклицательных и вопросительных предложений соответственно
-//    while (s != "") {
-//        int pos = s.find_first_of(".!?");
-//        pr = s.substr(0, pos);
-//        s0 = set_word(pr);
-//        if (s[pos] == '.') pov.insert(s0.begin(), s0.end());
-//        if (s[pos] == '!') voskl.insert(s0.begin(), s0.end());
-//        if (s[pos] == '?') vopr.insert(s0.begin(), s0.end());
-//        s.erase(0, pos + 1);
-//    }
-//    // Находим объединение voskl и vopr
-//    set <string> set_vopr_voskl;
-//    set_vopr_voskl.insert(voskl.begin(), voskl.end());
-//    set_vopr_voskl.insert(vopr.begin(), vopr.end());
-//    // Находим разность pov и объединенного множеста
-//    set_difference(pov.begin(), pov.end(), set_vopr_voskl.begin(), set_vopr_voskl.end(), inserter(res, res.begin()));
-//    for (set <string> ::iterator i = res.begin(); i != res.end(); i++)
-//        cout << *i << " ";
-//    return 0;
-//}
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Employee {//создаем структоуру с данными
+    string фамилия;
+    string должность;
+    string дата_рождения;
+    int стаж_работы;
+    int зарплата;
+};
+
+void quicksort(vector<Employee>& arr, int L, int R) {//алгоритм быстрой сортировки. опорный элемент- зарплата сотрудника
+    int i = L;
+    int j = R;
+    int pivot = arr[(L + R) / 2].зарплата; // Выбираем опорный элемент
+
+    while (i <= j) {
+        while (arr[i].зарплата < pivot) {
+            i++;
+        }
+        while (arr[j].зарплата > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if (L < j) {
+        quicksort(arr, L, j); // Сортируем левый подмассив
+    }
+    if (i < R) {
+        quicksort(arr, i, R); // Сортируем правый подмассив
+    }
+}
+
+int main() {
+    ifstream input_file("данные_о_сотрудниках.txt");//открываем файлы ввода и вывода
+    ofstream output_file("отсортированные_данные.txt");
+
+    vector<Employee> employees;//информация добавляется в вектор структуры
+    string line;
+
+    while (getline(input_file, line)) {
+        istringstream iss(line);
+        Employee employee;
+        getline(iss, employee.фамилия, ',');
+        getline(iss, employee.должность, ',');
+        getline(iss, employee.дата_рождения, ',');
+        iss >> employee.стаж_работы;
+        iss.ignore(); // Пропускаем запятую
+        iss >> employee.зарплата;
+        employees.push_back(employee);
+    }
+
+    quicksort(employees, 0, employees.size() - 1);//вызов функции
+
+    for (const Employee& employee : employees) {
+        output_file << employee.фамилия << ", " << employee.должность << ", " << employee.дата_рождения << ", " << employee.стаж_работы << ", " << employee.зарплата << "\n";
+    }
+
+    input_file.close();
+    output_file.close();
+
+    return 0;
+}
