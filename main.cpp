d161be4158440c8a74dd4af5ecaed14fc34ad199
Ani <ansahakyan1500@gmail.com>
2024-05-26 12:51:34 +0400

    Дан неориентированный граф. Определить содержит ли он циклы. Если да, то вывести их

--- main.cpp
+++ main.cpp
@@ -1,65 +1,122 @@
-#include <iostream> // Подключение библиотеки для ввода/вывода
-#include <fstream> // Подключение библиотеки для работы с файлами
-#include <list> // Подключение библиотеки для работы со списками
-#include <map> // Подключение библиотеки для работы с отображениями
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <map>
 
-using namespace std; // Использование пространства имен std
+using namespace std;
 
-map<int, list<int>> input_adj_list() // Функция для создания списка смежности
+vector<vector<int>> input_adj_list(int N) // создание списка смежности
 {
-    ifstream in("input.txt"); // Открытие файла "input.txt" для чтения
-    map<int, list<int>> l_Adj; // Создание отображения для хранения списка смежности
-    int x, y; // Переменные для хранения вершин
-    while (in.peek() != EOF) // Чтение файла до конца
+    ifstream in("input.txt");
+    vector<vector<int>> Gr;
+    Gr.resize(N);
+    int x, y;
+    while (in.peek() != EOF)
     {
-        in >> x >> y; // Считывание пары вершин
-        auto it_x = l_Adj.find(x); // Поиск вершины x в списке смежности
-        bool fl = true; // Флаг для проверки наличия ребра между вершинами
-        if (it_x != l_Adj.end()) // Если вершина x уже есть в списке смежности
+        in >> x >> y;
+        if (x > N || y > N)
+            continue;
+        else
         {
-            for (auto it_y = it_x->second.begin(); it_y != it_x->second.end(); it_y++) // Проверка наличия вершины y среди смежных вершин x
-                if (*it_y == y) // Если вершина y уже смежная с вершиной x
-                {
-                    fl = false; // Устанавливаем флаг в false
-                    break;
-                }
-        }
-        if (fl) // Если ребра между вершинами нет
-        {
-            l_Adj[x].push_back(y); // Добавляем вершину y к списку смежности вершины x
-            l_Adj[y].push_back(x); // Добавляем вершину x к списку смежности вершины y
+            Gr[x].push_back(y);
+            Gr[y].push_back(x);
         }
     }
-    return l_Adj; // Возвращаем список смежности
+    for (int i = 0; i < N; i++)
+        Gr[i].erase(unique(Gr[i].begin(), Gr[i].end()), Gr[i].end());
+    return Gr;
 }
 
-void output_adj_list(map<int, list<int>> l_Adj) // Функция для вывода списка смежности
+void output_adj_list(vector<vector<int>> Gr) // вывод списка смежности
 {
-    for (auto it = l_Adj.begin(); it != l_Adj.end(); it++) // Перебор всех вершин в списке смежности
+    for (int i = 0; i < Gr.size(); i++)
     {
-        cout << it->first << " : "; // Выводим номер вершины
-        for (auto it_1 = it->second.begin(); it_1 != it->second.end(); it_1++) // Перебор всех смежных вершин текущей вершины
-            cout << *it_1 << "; "; // Выводим номера смежных вершин
-        cout << endl; // Переход на новую строку
+        cout << i << ": ";
+        for (int vertex_adj : Gr[i])
+        {
+            cout << vertex_adj << ' ';
+        }
+        cout << endl;
     }
-    cout << endl; // Пустая строка для разделения вывода
+    cout << endl;
 }
 
-void result(map<int, list<int>> l_Adj) // Функция для вывода степени каждой вершины
+const int N = 7;
+vector<vector<int>> Gr = input_adj_list(N);
+int pr[N];
+int used[N];
+vector<vector<int>> cycles;
+vector<vector<int>> cycles_sort;
+
+void add_cycles(int start, int end) // добавление цикла в векторы
 {
-    for (auto it = l_Adj.begin(); it != l_Adj.end(); it++) // Перебор всех вершин в списке смежности
+    int cur = end; // переменная для перебора
+    vector<int> tmp;
+    while (cur != start)
     {
-        int count = 0; // Счетчик степени вершины
-        for (auto it_1 = it->second.begin(); it_1 != it->second.end(); it_1++) // Перебор всех смежных вершин текущей вершины
-            count++; // Увеличиваем счетчик на 1 за каждое ребро
-        cout << it->first << ": " << count << endl; // Выводим номер вершины и ее степень
+        tmp.push_back(cur); // начинаем перебирать предков
+        cur = pr[cur];      // и запоминаем путь
+    }
+    tmp.push_back(start);
+    reverse(tmp.begin(), tmp.end()); // приводим путь в прямой порядок
+    cycles.push_back(tmp);           // оригинальный цикл
+    sort(tmp.begin(), tmp.end());
+    cycles_sort.push_back(tmp); // отсортированный цикл
+}
+
+void find_cycles(int x)
+{
+    used[x] = 1; // пометили вершину
+    for (int i = 0; i < Gr[x].size(); i++)
+    {
+        if (pr[Gr[x][i]] == x) // вершина Gr[x][i] является предком x
+            continue;          // пропускаем
+        if (used[Gr[x][i]] == 0)
+        {                          // есть не посещенная вершина
+            pr[Gr[x][i]] = x;      // помечаем предка
+            find_cycles(Gr[x][i]); // ищем цикл начиная с неё
+        }
+        else if (used[Gr[x][i]] == 1) // уже посещали
+            add_cycles(Gr[x][i], x);  // запоминаем цикл
+    }
+    used[x] = 0; // очищаем помеченные вершины, выходя из рекурсии
+}
+
+void result() // нахождение циклов
+{
+    output_adj_list(Gr);
+
+    for (int i = 0; i < N; i++) // заполняем вспомогающие массивы
+    {
+        pr[i] = -1;
+        used[i] = 0;
+    }
+    find_cycles(0); // находим циклы начиная с нуля
+
+    map<vector<int>, int> CYCLES;
+    for (int i = 0; i < cycles_sort.size(); i++)
+    {
+        if (cycles_sort[i].size() > 2 && CYCLES.count(cycles_sort[i]) == 0) // цикл не состоит из одного ребра
+        {
+            CYCLES[cycles_sort[i]] = i; // запоминаем номера нужных векторов, используя отсортированные циклы
+        }
+    }
+    for (auto it = CYCLES.begin(); it != CYCLES.end(); it++)
+    {
+        int i = it->second;
+        cout << "cycle: ";
+        for (int j = 0; j < cycles[i].size(); j++)
+        {
+            cout << cycles[i][j] << ' '; // зная номера нужных вектором, выводим сами же циклы
+        }
+        cout << endl;
     }
 }
 
 int main()
 {
-    map<int, list<int>> l_Adj = input_adj_list(); // Создаем список смежности из файла
-    output_adj_list(l_Adj); // Выводим список смежности
-    result(l_Adj); // Выводим степень каждой вершины
-    return 0; // Возвращаем 0, что означает успешное завершение программы
+    result();
+    return 0;
 }
